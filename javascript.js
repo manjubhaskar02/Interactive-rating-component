15a825cb65cb4d479c5d8534dd931d30011ccca7
manjubhaskar02 <manju.bhaskar.jaya@gmail.com>
2023-09-28 14:10:04 +0530

    updated javascript.js

--- javascript.js
+++ javascript.js
@@ -9,48 +9,32 @@ const sentences = document.querySelectorAll(".sentence");
 
 
 submitButton.addEventListener("click", () => {
-    credit.classList.remove("hidden")
+    credit.classList.remove("hidden");
     mainContainer.style.display = "none";
+
     sentences.forEach((sentence, i) => {
         setTimeout(() => {
-            sentence.classList.remove("sentence")
-        }, i * 1200)
-
+            sentence.classList.remove("sentence");
+        }, i * 1000)
     })
-
-
 
     starContainer.innerHTML = "";
     for (let i = 0; i < rating.innerHTML; i++) {
-        setTimeout(() => {
-            starContainer.innerHTML += ` <img class="star animate-[pulse_1s_ease-out_infinite] " src="images/icon-star.svg" alt="star">`
-        }, i * 1000)
+        starContainer.innerHTML += ` <img class="star animate-[pulse_1s_ease-out_infinite] " src="images/icon-star.svg" alt="star">`
     }
 })
+
 rates.forEach((rate) => {
     rate.addEventListener("click", () => {
         rating.innerHTML = rate.innerHTML
     })
 })
 
-
-
-
 ratingButton.addEventListener("click", () => {
     mainContainer.style.display = "block";
-
     credit.classList.add("hidden");
+
     sentences.forEach((sentence, i) => {
-        sentence.classList.add("sentence")
-
+        sentence.classList.add("sentence");
     })
-
-
-
 })
-
-// sentences.forEach((sentence,i)=>{
-//     setTimeout(()=>{
-//     sentence.classList.remove("sentence");
-//         },i*500)
-// })
